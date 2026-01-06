7614fa64e2f1c2b35bf86a775e4680fcf3fa3471
Ahmad Siddique <asshikrani66@gmail.com>
2026-01-06 07:14:37 +0500

    me route for profile has been completed.

---
+++ src/app/api/user/me/route.ts
@@ -0,0 +1,21 @@
+import { getDataFromToken } from "@/helper/getDataFromToken";
+import User from "@/models/user";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request:NextRequest) {
+    try {
+        const userId = await getDataFromToken(request)
+
+        const user = await User.findOne({_id: userId}).select("-password")
+
+        if (!user) return NextResponse.json({message: "User not Found", data: ""})
+
+        return NextResponse.json({
+            message: "User Found",
+            data: user
+        })
+    } catch (error) {
+        return NextResponse.json({message: "Some Error Occured while geting User", data: ""})
+    }
+    
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import { Activity } from "react";
 
 export default function Home() {
   return (
     <div>
-      
+      <Activity mode={1 !== 1 ? "visible" : "hidden"}>
+        <h1 className="bg-amber-300 text-transparent text-5xl bg-clip-text bg-linear-to-b shadow-lg text-center my-auto max-w-[90vw] mx-auto max-h-[40vh] rounded-lg mt-44">
+        Am I visible
+      </h1>
+      </Activity>
     </div>
   );
 }
